acaa4beb142f7d878d74a2620006e8ef2448569b
Nagendar-java <115471824+Nagendar-java@users.noreply.github.com>
2023-01-04 16:24:36 +0530

    Create MR.java

---
+++ Java8_Adavanced/src/com/java/java8/MR.java
@@ -0,0 +1,16 @@
+package com.java.java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MR {
+	public static void main(String[] args) {
+		
+	
+	List<String> myList= Arrays.asList("test1", "test2", "test3","test1");
+	
+	myList.stream().sorted().forEach(System.out::println);;
+	
+	}
+	
+}

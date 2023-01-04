2f93e04ebd7d279683aeaf3b7ae069844842056e
Nagendar-java <115471824+Nagendar-java@users.noreply.github.com>
2023-01-04 16:11:04 +0530

    Create RemoveDuplicatesFromList.java

---
+++ Java8_Adavanced/src/com/java/java8/RemoveDuplicatesFromList.java
@@ -0,0 +1,14 @@
+package com.java.java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RemoveDuplicatesFromList {
+
+	public static void main(String[] args) {
+		List<Integer> myList= Arrays.asList(1,2,2,3,4,4,5,6,7,7,8,12);
+	//	myList.stream().collect(Collectors.toSet()).forEach(System.out::println);
+		myList.stream().distinct().collect(Collectors.toList()).forEach(System.out::println);
+	}
+}
